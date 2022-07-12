94a7b3f8cb45787f9295a10a5d56a20c8494120e
Vit <razzorenov.vit@yandex.ru>
2022-07-12 18:57:28 +0300

    Решение первой задачи

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,5 +1,17 @@
 ﻿// Задача 1: Напишите программу, которая на входе принимает два числа и выводит, кокое число болшее, а которое меньшее.
-int [] nums = {0, 1, 2, 3, 5};
+int [] nums = {3, 5};
 int max = nums.Max();
 Console.WriteLine (max);
 
+// Вариант 2 
+
+int a = (8);
+int b = (5);
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+     Console.WriteLine(b);
+}
