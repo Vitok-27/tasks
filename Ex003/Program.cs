0739fe1dfde96afd5e9a6918254051a993988f0c
Vit <razzorenov.vit@yandex.ru>
2022-07-12 19:03:36 +0300

    Решение третий задачи

---
+++ Ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным.
+int x=(5);
+x = Console.Read();
+if ((x % 2) == 0)
+{
+    Console.WriteLine("chentoe");
+}
+else
+{
+    Console.WriteLine("nechentoe");
+
+}
