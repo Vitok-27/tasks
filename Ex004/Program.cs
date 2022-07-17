c434a0cf8837b413d2551ff3c6d320f2b79a891a
Vit <razzorenov.vit@yandex.ru>
2022-07-17 14:47:54 +0300

    Решение четвертой задачи

---
+++ Ex004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int a = (254);
+int b = (a % 100 / 10) ;
+Console.WriteLine(b);
+
+//Вриант 2
+int num= (256);
+string NumberText = Convert.ToString(num);
+if (NumberText.Length > 2){
+  Console.WriteLine("вторая цифра " + NumberText[1]);
+}
