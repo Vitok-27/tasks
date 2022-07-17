5a00e7fd080e357441822f4040cd2d2ef776508d
Vit <razzorenov.vit@yandex.ru>
2022-07-17 14:49:18 +0300

    Решение пятой задачи

---
+++ Ex005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int num= (456);
+string NumberText = Convert.ToString(num);
+if (NumberText.Length > 2){
+  Console.WriteLine("третья цифра " + NumberText[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
