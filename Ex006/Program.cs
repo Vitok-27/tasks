a2e83d6c7eede61a2052f1cc2bc647a95e8d68eb
Vit <razzorenov.vit@yandex.ru>
2022-07-17 14:49:41 +0300

    Решение шестой  задачи

---
+++ Ex006/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int a;
+a = 4;
+switch (a)
+
+{
+    case 1: 
+    Console.WriteLine ("Понедельник");
+    break;
+    
+    case 2: 
+    Console.WriteLine ("Вторник");
+    break;
+    
+    case 3: 
+    Console.WriteLine ("Среда");
+    break;
+    
+    case 4: 
+    Console.WriteLine ("Четверг");
+    break;
+    
+    case 5: 
+    Console.WriteLine ("Пятница");
+    break;
+    
+    case 6: 
+    Console.WriteLine ("Суббота");
+    break;
+    
+    case 7: 
+    Console.WriteLine ("Воскресенье");
+    break;
+    
+    default:
+    
+    Console.WriteLine ("Ошибка");
+    break;
+}
