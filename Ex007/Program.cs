187cc8316241f67df548b04e695838455536b872
Vit <razzorenov.vit@yandex.ru>
2022-07-20 19:53:05 +0300

    7 задача

---
+++ Ex007/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите чилсо");
+int n = Convert.ToInt32 (Console.ReadLine());
+int a = n;
+int b = (0);
+int c = (0);
+int d = (0);
+while ( n > 0 )
+{
+b = (n % 10); 
+c = (n % 100 / 10); 
+d = (n / 10); 
+}
+if ( n == a)
+Console.WriteLine("YES");
+else 
+Console.WriteLine("NO");
