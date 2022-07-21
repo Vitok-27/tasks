217f574041cad7c701f351c90285ef0e7a169b74
Vit <razzorenov.vit@yandex.ru>
2022-07-21 20:21:58 +0300

    Решение девятой  задачи

---
+++ Ex009/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+Console.WriteLine("{0}^2 = {1}", i, i*i);
+Console.ReadKey(true);
