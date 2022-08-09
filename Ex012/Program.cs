b7c8d7cfb149a8dc8459b3dc699bb64d4e545152
Vit <razzorenov.vit@yandex.ru>
2022-08-09 15:22:54 +0300

    Решение двенадцотой задачи

---
+++ Ex012/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите метод(ы), которай задаёт массив из 8 элементов и выводит их на экран.
+Console.Write("Введите количество элементов массива:");
+int a= int.Parse(Console.ReadLine());
+int [] array= new int [a];
+for (int i=0; i<array.Length; i++)
+{
+Console.Write("Введите элемент массива под индексом {i}:");
+array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива:");
+for (int i=0; i<array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
