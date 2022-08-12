25be403e0de91963353d7a4d210e1660bfb05f36
Vit <razzorenov.vit@yandex.ru>
2022-08-12 16:00:36 +0300

    Решение тринадцатой задачи

---
+++ Ex013/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] a = new int[3];
+for (int i = 0; i < a.Length; i++)
+{
+    Console.Write($"Введите a[{i}] = ");
+    a[i] = int.Parse(Console.ReadLine());
+}
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i] % 2 == 1)
+        sum += a[i];
+}
+Console.WriteLine($"Сумма нечетных чисел = {sum}");
