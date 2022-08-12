0c5c926543f72ffcb69d6a7269ee63f628acf6c1
Vit <razzorenov.vit@yandex.ru>
2022-08-12 16:01:01 +0300

    Решение четырнадцотой задачи

---
+++ Ex014/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+double min=double.MaxValue;
+double max=double.MinValue;
+double a;
+for(int i=0;i<2;i++)
+{
+    Console.Write("Введите число: ");
+    a=double.Parse(Console.ReadLine());
+    if (a>max)
+    max=a;
+    if(a<min)
+    min=a;
+}
+Console.WriteLine(max-min);
+Console.ReadKey();
