ee345b45af39037e96e9ae8ce9d8215c8546b565
Vit <razzorenov.vit@yandex.ru>
2022-08-16 17:08:55 +0300

    Решение пятнадцатой задачи

---
+++ Ex015/Program.cs
@@ -0,0 +1,12 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите колличество чисел: ");
+int n = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int a = int.Parse(Console.ReadLine());
+    if (a > 0)
+        count++;
+}
+Console.WriteLine($" чисел больше 0: {count}");
