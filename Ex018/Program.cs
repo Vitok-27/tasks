3b4d6c2b9f358723233a6f9c007630d20ea892c0
Vit <razzorenov.vit@yandex.ru>
2022-08-19 17:05:48 +0300

    Решение восемнадцатой  задачи

---
+++ Ex018/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Console.Write("Введите строку:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбец:");
+int columns = Convert.ToInt32(Console.ReadLine());
+int n = 3; // размер массива
+int m = 4; // размер массива
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+if (rows< 0 | rows> arr.GetLength(0)|columns<0| columns>arr.GetLength(1))
+{
+Console.WriteLine("Не существует");
+}
+else
+{
+Console.WriteLine("Значение элемента массива = {0}", arr[rows,columns]);
+}
+Console.ReadLine();
+        
