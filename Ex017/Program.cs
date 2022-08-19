f32465fdae7d16b3b768b2a9d496cfef33f75c9e
Vit <razzorenov.vit@yandex.ru>
2022-08-19 17:05:03 +0300

    Решение семнадцатой  задачи

---
+++ Ex017/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,] a = new double[3, 3];
+Random random = new Random();
+for (int m=0; m<3;m++)
+{
+    for (int n=0; n<3; n++)
+    {
+        a[m,n] = random.Next(10);
+        Console.Write("{0,2}", a[m,n]);
+    }
+    Console.WriteLine();
+    }
